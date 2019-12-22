55c4fd3e6a1dbd163fa90627d7b2885dc8a648c2
Hernan Peralta <alegodemencia@gmail.com>
2019-12-22 03:00:26 -0300

    Improves event handling

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,12 @@ let $isDarken = document.querySelector("#is-darken");
 let darkenActivated = false;
 
 
-
 function drawCanvas(squares){
-    for (let i = 0; i<squares; i++){
+    for (let i = 0; i < squares; i++) {
         let div = document.createElement("div");
         $container.appendChild(div).classList.add("row");
-        
-        for (let j = 0; j<squares; j++){
+
+        for (let j = 0; j < squares; j++) {
             let div2 = document.createElement("div");
             $container.lastElementChild.appendChild(div2).classList.add("cell");
         }
@@ -24,45 +23,51 @@ function drawCanvas(squares){
 }
 
 
-function resetCanvas(){
-    for(let i=0; i<$cells.length; i++){
-        $cells[i].style.backgroundColor = 'white';
+function resetCanvas() {
+    for (let i = 0; i < $cells.length; i++) {
+        $cells[i].style.backgroundColor = "white";
         $cells[i].style.filter = "";
     }
 }
 
 
-function paint(){
-    $cells = document.querySelectorAll(".cell");
-    
-    if (randomActivated === true){
-        $cells.forEach(cell => cell.addEventListener('mouseover', function(){randomColor(cell)}));
-    }
-    
-    if (darkenActivated === true){
-        $cells.forEach(cell => cell.addEventListener('mouseover', function(){darken(cell)}));
-    }
+function paint() {
+    $cells = document.querySelectorAll(".cell")
 
+    $cells.forEach(cell =>cell.addEventListener("mouseover", function(){handlePaint(cell)}));
+}
+
+
+function handlePaint(cell) {
+    if (randomActivated === true){
+        randomColor(cell);
+    } 
+    if (darkenActivated === true){
+        darken(cell);
+    } 
     else {
-        $cells.forEach(cell => cell.addEventListener('mouseover', function(){userSelectedColor(cell)}));
+        userSelectedColor(cell);
     }
 }
 
 
-function userSelectedColor(cell){
+function userSelectedColor(cell) {
     let colorSelected = document.querySelector("#color-selector").value;
     cell.style.backgroundColor = colorSelected;
+    cell.style.filter = "";
 }
 
 
-function randomColor(cell){
-    let randomizedColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);}) // https://stackoverflow.com/questions/5092808/how-do-i-randomly-generate-html-hex-color-codes-using-javascript
+function randomColor(cell) {
+    let randomizedColor = "#000000".replace(/0/g, function () {
+        return (~~(Math.random() * 16)).toString(16)}) // https://stackoverflow.com/questions/5092808/how-do-i-randomly-generate-html-hex-color-codes-using-javascript
     cell.style.backgroundColor = randomizedColor;
+    cell.style.filter = "";
 }
 
 
 function darken(cell){
-    if (cell.style.filter.includes("brightness")===false){
+    if (cell.style.filter.includes("brightness") === false){
         cell.style.filter = "brightness(90%)";
     }
 
@@ -70,13 +75,12 @@ function darken(cell){
         return "";
     }
 
-    // if (cell.style.filter.includes("brightness(100%)")){
-    //     return "";
-    // }
-
+  // if (cell.style.filter.includes("brightness(100%)")){
+  //     return "";
+  // }
     else{
         let brightness = cell.style.filter.slice(11, 13);
-        brightness = Number(brightness)-10;
+        brightness = Number(brightness) - 10;
         cell.style.filter = "brightness(" + brightness + "%)";
         console.log(cell.style.filter);
     }
@@ -85,27 +89,29 @@ function darken(cell){
 
 function newCanvas(){
     randomActivated = false;
-    $container.innerHTML = '';
+    $container.innerHTML = "";
     let squares = Number(prompt("Enter the size of the canvas"));
     drawCanvas(squares);
-    let height = (700/squares)-2;//I subtract 2 because of the 1px border of each cell
-    document.querySelectorAll(".cell").forEach(cell => cell.setAttribute('style', 'height:'+height+'px;'));
+    let height = 700 / squares - 2; //I subtract 2 because of the 1px border of each cell
+    document
+      .querySelectorAll(".cell")
+      .forEach(cell => cell.setAttribute("style", "height:" + height + "px;"))
 
-    paint();  
+    paint();
 }
 
 
 $newCanvas.onclick = newCanvas;
 $resetCanvas.onclick = resetCanvas;
 
-$randomColor.onclick = function(){
+$randomColor.onclick = function (){
     randomActivated = !randomActivated;
-    $isRandom.innerText = 'Random color activated: ' + `${randomActivated}`;
-    return paint()
-};
-
-$darken.onclick = function(){
-    darkenActivated = !darkenActivated;
-    $isDarken.innerText = 'Darken activated: ' + `${darkenActivated}`;
+    $isRandom.innerText = "Random color activated: " + `${randomActivated}`;
     return paint();
-};
+}
+
+$darken.onclick = function () {
+    darkenActivated = !darkenActivated;
+    $isDarken.innerText = "Darken activated: " + `${darkenActivated}`;
+    return paint();
+}
